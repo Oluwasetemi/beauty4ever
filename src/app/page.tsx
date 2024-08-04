aa4661e2e13d2c8212e58a42f7d2ad093273f3ee
ojo oluwasetemi <setemiojo@gmail.com>
2024-08-03 21:02:40 -0500

    Remove drizzle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Working</h1>
+      <h1>Landing Page</h1>
     </main>
   );
 }
